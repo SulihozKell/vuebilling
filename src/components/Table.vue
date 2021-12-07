ecfb2301b0624af24761c9ee10729e3758a85892
Bendeguz <bendeguz@medea.hu>
2021-12-07 14:29:40 +0100

    Kezdetleges táblázat kiiratása.

---
+++ src/components/Table.vue
@@ -0,0 +1,35 @@
+<template>
+  <table>
+    <TableItem
+      v-for="row in rows"
+      v-bind:key="row.title"
+      :row="row"
+      />
+  </table>
+</template>
+
+<script>
+import TableItem from "./TableItem.vue"
+export default {
+  props: ["rows"],
+  components: {TableItem},
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/TableItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <tr>
+        <th>{{title}}</th>
+        <td>{{price}}</td>
+        <td>{{quantity}}</td>
+    </tr>
+</template>
+
+<script>
+export default {
+    props: ["row"],
+    data() {
+        return {
+            title: this.row.title,
+            price: this.row.price,
+            quantity: this.row.quantity,
+        }
+    }
+}
+</script>
